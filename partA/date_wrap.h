92342a7732276cf8017dff9f872066b94ee20bea
Lior Bar <liorbar03@gmail.com>
2021-01-08 00:22:34 +0200

    17 function

---
+++ partA/date_wrap.h
@@ -0,0 +1,32 @@
+#include <iostream> 
+using namespace std;
+extern "C" {
+    #include "date.h"
+}
+
+class DateWrap { 
+    Date date;
+public:
+	DateWrap(int day, int month, int year);
+	DateWrap(const DateWrap& date);
+    ~DateWrap();
+    DateWrap& operator=(const DateWrap&);
+    int day() const;
+    int month() const;
+    int year() const;
+    friend bool operator==(const DateWrap& date1, const DateWrap& date2);
+    friend bool operator<(const DateWrap& date1, const DateWrap& date2);
+    DateWrap operator++(int);
+    class NegativeDays{};
+    class InvalidDate{}; 
+    class AllocationFailed{}; 
+};
+DateWrap& operator+=(DateWrap& date, int days); 
+DateWrap operator+(const DateWrap& date, int days);
+DateWrap operator+(int days, const DateWrap& date);
+ostream& operator<<(ostream& out, const DateWrap& date); 
+bool operator!=(const DateWrap& date1, const DateWrap& date2);
+bool operator<=(const DateWrap& date1, const DateWrap& date2);
+bool operator>(const DateWrap& date1, const DateWrap& date2);
+bool operator>=(const DateWrap& date1, const DateWrap& date2);
+

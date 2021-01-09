1b002b47044dcb82bd01b575a75fcb9b0e026367
Lior Raskin <lior.raskin@campus.technion.ac.il>
2021-01-09 20:19:55 +0200

    generic queue partB, made basic functions

--- partB/generic_queue.h
+++ partB/generic_queue.h
@@ -1,3 +1,5 @@
+#ifndef GENERIC_QUEUE_H_
+#define GENERIC_QUEUE_H_
 #include <iostream>
 namespace mtm{
     template<class T>
@@ -21,18 +23,81 @@ namespace mtm{
                 current_pointer = NULL; 
                 first = NULL; 
             }
-
+            ~PriorityQueue<T>()
+            {
+                while(next_pointer!=NULL)
+                {
+                   delete(current_pointer);
+                   current_pointer = next_pointer;
+                   next_pointer = next_pointer->next;  
+                }
+                delete(current_pointer);
+            }
+            PriorityQueue<T>(const PriorityQueue<T>& queue_to_copy)
+            {
+                while(queue_to_copy.current_pointer!=NULL)
+                {
+                   current_pointer = new Node<T>;
+                   current_pointer->data = queue_to_copy.current_pointer->data; 
+                   current_pointer = current_pointer->next; 
+                   queue_to_copy.current_pointer = queue_to_copy.current_pointer->next; 
+                }
+                queue_to_copy.current_pointer = queue_to_copy.first;
+                if(queue_to_copy.first != NULL)
+                {
+                   queue_to_copy.next_pointer = queue_to_copy.current_pointer->next; 
+                }
+                else
+                {
+                   queue_to_copy.next_pointer = NULL;  
+                }
+                first = queue_to_copy.first;
+                current_pointer = queue_to_copy.current_pointer;
+                next_pointer = queue_to_copy.next_pointer; 
+            }
+            PriorityQueue<T>& operator=(const PriorityQueue<T>& queue_to_copy)
+            {
+                if(this == &queue_to_copy)
+                {
+                    return *this;
+                }
+                while(next_pointer!=NULL)
+                {
+                   delete(current_pointer);
+                   current_pointer = next_pointer;
+                   next_pointer = next_pointer->next;  
+                }
+                delete(current_pointer);
+                while(queue_to_copy.current_pointer!=NULL)
+                {
+                   current_pointer = new Node<T>;
+                   current_pointer->data = queue_to_copy.current_pointer->data; 
+                   current_pointer = current_pointer->next; 
+                   queue_to_copy.current_pointer = queue_to_copy.current_pointer->next; 
+                }
+                queue_to_copy.current_pointer = queue_to_copy.first;
+                if(queue_to_copy.first != NULL)
+                {
+                   queue_to_copy.next_pointer = queue_to_copy.current_pointer->next; 
+                }
+                else
+                {
+                   queue_to_copy.next_pointer = NULL;  
+                }
+                first = queue_to_copy.first;
+                current_pointer = queue_to_copy.current_pointer;
+                next_pointer = queue_to_copy.next_pointer; 
+                return *this;
+            }
             void addElement(T data) 
             {
                 if(first == NULL) 
                 {
                     // The list is empty
-                    next_pointer = new Node<T>;
-                    next_pointer->data = data; // assuming T has a copy constructor. 
-                    current_pointer = next_pointer;
-                    next_pointer->next = NULL;
+                    current_pointer = new Node<T>;
+                    current_pointer->data = data; // assuming T has a copy constructor. 
+                    current_pointer->next = NULL;
                     first = current_pointer;
-
                 } 
                 else 
                 {
@@ -40,19 +105,20 @@ namespace mtm{
                    if(data < current_pointer->data) // data smaller than first node in list 
                     {
                         current_pointer = new Node<T>;
-                        current_pointer->data = data; // assuming T has a copy constructor.
+                        current_pointer->data = data; 
+                        current_pointer->next = first;
+                        first = current_pointer;
+                        next_pointer = first->next; 
                     }
-                    current_pointer->next = next_pointer;
-                    first = current_pointer;
                     else
                     {
                         // The list has more than one element
                         while (next_pointer!= NULL)
                         { 
-                            if(data < next_pointer->data && current_pointer->data <data)
+                            if(data < next_pointer->data && current_pointer->data <data) // asumming T has '<' operator.
                             {
                                 Node<T>* temp_pointer = new Node<T>;
-                                temp_pointer = data; // assuming T has a copy constructor.
+                                temp_pointer = data; 
                                 current_pointer->next = temp_pointer;
                                 temp_pointer->next = next_pointer;
                                 break; 
@@ -63,7 +129,7 @@ namespace mtm{
                         if(next_pointer == NULL && current_pointer->data < data) 
                         {
                             next_pointer = new Node<T>;
-                            next_pointer->data = data; // assuming T has a copy constructor. 
+                            next_pointer->data = data; 
                             next_pointer->next = NULL; 
                             current_pointer->next = next_pointer; 
                         }
@@ -72,35 +138,71 @@ namespace mtm{
                     }
                 }
             }
-        void deleteElement (T data)
-        {
-            while(cu)
-        }
 
-
-        T get(int index) 
-        {
-            if(index == 0) 
+            void removeElement(T data)
             {
-                // Get the iterator element
-                return this->iterator->data;
-            } else 
-            {
-                // Get the index'th element
-                Node<T>*  next_pointer = this->iterator;
-                for(int i = 0; i < index; ++i) 
-                {
-                    curr = curr->next;
-                }
-                return curr->data;
+               if(first != NULL)
+               {
+                    if(first->data == data)
+                    {
+                        Node<T>* temp_pointer = first;
+                        first = frist->next; 
+                        delete(temp_pointer);
+                    }
+                    else
+                    {
+                        while(next_pointer!=NULL)
+                        {
+                            if(next_pointer=>data == data)
+                            {
+                                Node<T>* temp_pointer = next_pointer;
+                                next_pointer = next_pointer->next;
+                                current_pointer->next = next_pointer; 
+                                delete(temp_pointer);
+                            }
+                        }
+                    }
+                    current_pointer = first; 
+                    if(current_pointer !=NULL)
+                    {
+                        next_pointer = current_pointer->next; 
+                    }
+                    else
+                    {
+                        next_pointer =  NULL; 
+                    }
+                    
+               }
             }
-        }
+            Node<T>* getIterator()
+            {
+                iterator == first; 
+                return iterator; 
+            }
 
-        T operator[](int index) 
-        {
-            return get(index);
-        }
+            //not sure if we sholud have const version or regular one or both. 
+            
+            bool containsElement(T data)
+            {
+                while(current_pointer!=)
+                {
+                    if(current_pointer->data == data)
+                    {
+                        reutrn true; 
+                    }
+                }
+                current_pointer = first; 
+                return false; 
+            }
 
+            T getFirstData() 
+            {
+                return first->data; 
+            }
+            const T getFirstData() 
+            {
+                return first->data; 
+            }
     };
 }
-
+#endif
